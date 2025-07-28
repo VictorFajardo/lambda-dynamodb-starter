20198745dbb2299475f23c7e2bb6139f7e440508
Victor Fajardo <fajardo.de.leon@gmail.com>
2025-07-28 15:59:02 -0400

    ✨ add test for app-stack.ts

---
+++ lib/__tests__/app-stack.test.ts
@@ -0,0 +1,42 @@
+import { App } from 'aws-cdk-lib';
+import { Template, Match } from 'aws-cdk-lib/assertions';
+import { AppStack } from '../app-stack';
+
+describe('AppStack', () => {
+    const app = new App();
+    const stack = new AppStack(app, 'TestStack');
+    const template = Template.fromStack(stack);
+
+    it('creates a DynamoDB table with id partition key', () => {
+        template.hasResourceProperties('AWS::DynamoDB::Table', {
+            KeySchema: [
+                { AttributeName: 'id', KeyType: 'HASH' }
+            ],
+            AttributeDefinitions: [
+                { AttributeName: 'id', AttributeType: 'S' }
+            ]
+        });
+    });
+
+    it('creates 5 Lambda functions', () => {
+        template.resourceCountIs('AWS::Lambda::Function', 5);
+    });
+
+    it('sets environment variable for all Lambdas', () => {
+        template.hasResourceProperties('AWS::Lambda::Function', {
+            Environment: {
+                Variables: Match.objectLike({
+                    NOTES_TABLE: Match.anyValue(),
+                }),
+            },
+        });
+    });
+
+    it('creates API Gateway with /notes resource and methods', () => {
+        template.hasResourceProperties('AWS::ApiGateway::RestApi', {
+            Name: 'Notes Service',
+        });
+
+        template.resourceCountIs('AWS::ApiGateway::Method', 5); // POST, GET, GET /{id}, PUT, DELETE
+    });
+});
