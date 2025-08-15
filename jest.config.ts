382057f2141b5f9a448b3819fa950bc40166ef8a
Victor Fajardo <fajardo.de.leon@gmail.com>
2025-08-14 22:24:12 -0400

    ✨ add jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
   testMatch: ['**/__tests__/**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js'],
   rootDir: './',
+  setupFiles: ['<rootDir>/jest.setup.ts'],
 };
 
 export default config;
--- lambda/notes/get-all/handler.ts
+++ lambda/notes/get-all/handler.ts
@@ -1,13 +1,26 @@
 import { APIGatewayProxyResult } from 'aws-lambda';
+import AWSXRay from 'aws-xray-sdk-core';
 import { internalError, ok } from '../../utils/response';
 import { getAllNotes } from './service';
 
 export const handler = async (): Promise<APIGatewayProxyResult> => {
+  let subsegment;
+
   try {
+    const segment = AWSXRay.getSegment();
+    if (segment) {
+      subsegment = segment.addNewSubsegment('CustomLogicGetAllNotes');
+      subsegment.addAnnotation('operation', 'getAllNotes');
+    }
+
     const notes = await getAllNotes();
 
     return ok({ notes });
   } catch (error: unknown) {
     return internalError(error);
+  } finally {
+    if (subsegment) {
+      subsegment.close();
+    }
   }
 };
