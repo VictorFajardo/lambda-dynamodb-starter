abf832b8b99a7d91b8ab229d197cdbed09e6048a
Victor Fajardo <fajardo.de.leon@gmail.com>
2025-08-16 22:19:56 -0400

    👌 cdk build working in prod

--- lambda/notes/get-all/handler.ts
+++ lambda/notes/get-all/handler.ts
@@ -1,10 +1,18 @@
-import { APIGatewayProxyResult } from 'aws-lambda';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { ok } from '../../utils/response';
 import { getAllNotes } from './service';
 import { withSubsegment } from '../../utils/xray';
 import { handleError } from '../../utils/errorManager';
 
-export const handler = async (): Promise<APIGatewayProxyResult> => {
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  // Access Cognito claims
+  const claims = event.requestContext.authorizer?.claims;
+  console.log('Cognito Claims:', claims);
+
+  // Example: get email
+  const userEmail = claims?.email;
+  console.log('Authenticated user email:', userEmail);
+
   try {
     return await withSubsegment('CustomLogicGetAllNotes', async (sub) => {
       sub?.addAnnotation('operation', 'getAllNotes');
--- lambda/notes/options/handler.ts
+++ lambda/notes/options/handler.ts
@@ -1,5 +1,14 @@
 import { response } from '../../utils/response';
 
 export const handler = async () => {
-  return response(204, {});
+  return response(
+    204,
+    {},
+    {
+      'Access-Control-Allow-Origin': process.env.ALLOWED_ORIGIN ?? '*',
+      'Access-Control-Allow-Credentials': 'true',
+      'Access-Control-Allow-Headers': 'Content-Type,Authorization',
+      'Access-Control-Allow-Methods': 'OPTIONS,GET,POST,PUT,DELETE',
+    }
+  );
 };
