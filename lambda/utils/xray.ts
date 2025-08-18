9d68eba21688ea784b387dedc3f00fb9cc4c0c04
Victor Fajardo <fajardo.de.leon@gmail.com>
2025-08-17 22:24:30 -0400

    savepoit

--- lambda/utils/xray.ts
+++ lambda/utils/xray.ts
@@ -1,15 +1,18 @@
-import AWSXRay, { Subsegment } from 'aws-xray-sdk-core';
+import * as AWSXRay from 'aws-xray-sdk-core';
+import { Subsegment } from 'aws-xray-sdk-core';
 
 AWSXRay.setContextMissingStrategy('IGNORE_ERROR');
 
-export function withSubsegment<T>(
+export async function withSubsegment<T>(
   name: string,
   fn: (subsegment?: Subsegment) => Promise<T>
 ): Promise<T> {
   const segment = AWSXRay.getSegment?.();
   const subsegment = segment ? segment.addNewSubsegment(name) : undefined;
 
-  return fn(subsegment).finally(() => {
-    if (subsegment) subsegment.close();
-  });
+  try {
+    return await fn(subsegment);
+  } finally {
+    subsegment?.close();
+  }
 }
