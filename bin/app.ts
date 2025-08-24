677bfe2ea1880f12cd5fd2e59d574735dbb8c41b
Victor Fajardo <fajardo.de.leon@gmail.com>
2025-08-23 21:07:48 -0400

    � fix app.ts NODE_ENV

--- bin/app.ts
+++ bin/app.ts
@@ -1,5 +1,9 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import * as cdk from 'aws-cdk-lib';
 import { AppStack } from '../lib/app-stack';
+
+const envFile = process.env.NODE_ENV === 'local' ? `.env.${process.env.NODE_ENV}` : '.env';
+require('dotenv').config({ path: envFile });
 
 const app = new cdk.App();
 
